97144decf0fca5db59e14669115b32c2120d722d
leslie <leslie@producement.com>
2021-03-21 16:51:22 +0100

    code refactor

--- WalkingStore/src/main/java/com/sheke/walking/filter/JwtAuthenticationFilter.java
+++ WalkingStore/src/main/java/com/sheke/walking/filter/JwtAuthenticationFilter.java
@@ -1,9 +1,6 @@
 package com.sheke.walking.filter;
 
 import com.sheke.walking.utils.JwtUtilities;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
@@ -30,27 +26,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        log.info("in doFilterInternal");
         final String authorizationHeader = request.getHeader("Authorization");
-        String uri = request.getRequestURL().toString();
 
-//        if (uri.contains("/swagger") || uri.contains("/auth")) {
-//            request.getRequestDispatcher(request.getServletPath()).forward(request, res);
-//            log.info("forwarding response");
-//        }
         String username = null;
         String jwt = null;
-        log.info(uri);
-        log.info("Authorization header: " + authorizationHeader);
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+
+        if (authorizationHeader != null
+                && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             username = jwtUtils.extractUsername(jwt);
-            log.info("in filter, token: " + authorizationHeader);
         }
+
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
             if (jwtUtils.validateToken(jwt, userDetails)) {
-                log.info("validated? " + jwtUtils.validateToken(jwt, userDetails));
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
@@ -60,7 +49,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             }
         }
 
-        log.info("leaving do filter internal");
         filterChain.doFilter(request, response);
     }
 }
